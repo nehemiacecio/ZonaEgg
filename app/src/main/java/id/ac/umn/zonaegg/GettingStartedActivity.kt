537517c80c3d1d84d39ed4472379fafb7df1ea8e
Darren-34810 <darren.denisson@student.umn.ac.id>
2021-11-12 17:35:04 +0700

    Added new activity and resources

---
+++ app/src/main/java/id/ac/umn/zonaegg/GettingStartedActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.umn.zonaegg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GettingStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_getting_started)
+    }
+}
