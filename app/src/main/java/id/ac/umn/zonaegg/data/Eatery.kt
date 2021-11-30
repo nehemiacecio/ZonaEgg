7bbccbcd6ccd41d035efc05ea22ee46dab7f4f75
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2021-11-30 22:34:41 +0700

    Data Class Eatery

---
+++ app/src/main/java/id/ac/umn/zonaegg/data/Eatery.kt
@@ -0,0 +1,10 @@
+package id.ac.umn.zonaegg.data
+
+data class Eatery(
+    var id: String,
+    var name: String,
+    var category: String,
+    var rating: String,
+    var distance: Float,
+    var photoUrl: String
+)
